f4abf4400c89a9d05f5a16d1ae8c150c26b07cea
mass-inbound <development@massinbound.com>
2025-09-04 22:34:23 +0545

    favicon uploaded

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import {
   ScrollRestoration,
   useRouteLoaderData,
 } from 'react-router';
-import favicon from '~/assets/MY DISCOVER VACATIONS ICON COLOR.png';
+import favicon from '~/assets/favicon-discovery.svg';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
