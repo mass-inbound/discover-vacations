bea1309d414a9d88dbab67df900e019b5abcdf39
abiralaz <abiralkj.azminds@gmail.com>
2026-03-24 12:52:40 +0545

    test at main for deployment

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -202,7 +202,7 @@ export default function Homepage() {
               to="/discover-offers"
               className="flex items-center text-white underline text-[16px] md:text-[20px] font-[400] hover:text-gray-300"
             >
-              See All Offers
+              See All Offer
             </Link>
           </div>
         </div>
